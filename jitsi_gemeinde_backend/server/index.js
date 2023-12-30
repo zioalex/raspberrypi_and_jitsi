e27ef5e962af1bbcd0d2e81f07594f33ebfacb5e
Alessandro Surace <zioalex@gmail.com>
2023-12-30 13:24:50 +0100

    Fix backend

--- jitsi_gemeinde_backend/server/index.js
+++ jitsi_gemeinde_backend/server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../../jitsi_gemeinde_frontend/build')));
+//app.use(express.static(path.resolve(__dirname, '../../jitsi_gemeinde_frontend/build')));
 
 app.get('/shutdown', (req, res) => {
     // res.send(`stdout: SHUTDOWN Initiated`);
@@ -52,9 +52,9 @@ app.get('/status', (req, res) => {
 });  
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../jitsi_gemeinde_frontend/build', 'index.html'));
-});
+//app.get('*', (req, res) => {
+//  res.sendFile(path.resolve(__dirname, '../../jitsi_gemeinde_frontend/build', 'index.html'));
+//});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
